aaf6ebf9a910c40dda6ecf2de40a2f96ae92e0c5
Rubini Dhanasekaran <drubini90@gmail.com>
2018-10-14 22:30:54 -0700

    adding test

---
+++ Algorithms/src/main/java/DataStructure/Algorithms/App.java
@@ -0,0 +1,13 @@
+package DataStructure.Algorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Algorithms/src/test/java/DataStructure/Algorithms/sortArrayByParity.java
@@ -0,0 +1,11 @@
+package DataStructure.Algorithms;
+
+import org.junit.Test;
+
+public class sortArrayByParity {
+	@Test
+	public void sortTest() {
+	
+		System.out.println("Test");
+	}
+}

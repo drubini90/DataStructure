3442e2cb6e19d9df8d72c20c00197610202c03d3
Rubini Dhanasekaran <drubini90@gmail.com>
2018-10-14 23:06:16 -0700

    Added Sort Array by Parity - Leetcode 905

--- Algorithms/src/test/java/DataStructure/Algorithms/sortArrayByParity.java
+++ Algorithms/src/test/java/DataStructure/Algorithms/sortArrayByParity.java
@@ -1,11 +1,33 @@
 package DataStructure.Algorithms;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class sortArrayByParity {
 	@Test
-	public void sortTest() {
-	
-		System.out.println("Test");
+	public void testSortArrayByParity() {
+		int[] result = sortArrayByParity(new int[] {1,2,3,4});
+		Assert.assertArrayEquals(new int[] {2,4,3,1}, result);
 	}
+	public int[] sortArrayByParity(int[] A) {
+        
+        int inputSize = A.length;
+        int[] answer = new int[inputSize];
+        int j = 1;
+        int k = 0;
+        
+        for(int i = 0; i<inputSize; i++){
+            
+            if(A[i]%2 == 0){
+                answer[k] = A[i];
+                k++;
+            }
+            else{
+                answer[inputSize-j] = A[i];
+                j++;
+            }
+            
+        }
+        return answer;
+    }
 }
